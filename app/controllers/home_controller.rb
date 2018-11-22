06b896ab900653ba932b78fd35df1b81528f3afe
sugihararen <sugihara@dptr.jp>
2018-11-22 19:04:29 +0900

    予約一覧画面2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user, only: [:top]
 
   # トップ画面
-  def index; end
+  def index
+    @today = Date.today
+  end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   # トップ画面予約一覧
   get 'home/index' => 'home#index'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
