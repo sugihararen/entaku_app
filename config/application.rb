909ab425b45a7c55b46652d5ba9daee16a7a0bfb
sugihararen <sugihara@dptr.jp>
2018-11-28 17:06:45 +0900

    タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module EntakuApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.assets.initialize_on_precompile = false
 
     config.generators do |g|
