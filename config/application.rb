69108633f1ece3c01c0b89ef5f4c41d3be72c0e8
sugihararen <sugihara@dptr.jp>
2018-11-29 14:13:11 +0900

    i18n追加

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module EntakuApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -20,8 +19,8 @@ module EntakuApp
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.assets.initialize_on_precompile = false
-
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
