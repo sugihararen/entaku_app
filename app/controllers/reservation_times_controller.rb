5cb8d523f825373046661f0a242c17e29646047a
sugihararen <sugihara@dptr.jp>
2018-12-04 18:42:38 +0900

    同時刻で予約ができないようにする

--- app/controllers/reservation_times_controller.rb
+++ app/controllers/reservation_times_controller.rb
@@ -3,8 +3,12 @@ class ReservationTimesController < ApplicationController
   before_action :set_current_user
   def create
     @reservation_time = ReservationTime.new(reservation_time_params)
-    @reservation_time.update(user_id: session[:user_id])
-    if @reservation_time.save      
+    start_time = Time.zone.parse("#{reservation_time_params['start_time(1i)']}-#{reservation_time_params['start_time(2i)']}-#{reservation_time_params['start_time(3i)']} #{reservation_time_params['start_time(4i)']}:#{reservation_time_params['start_time(5i)']}:00").to_s       
+    if ReservationTime.find_by(reservation_date: reservation_time_params[:reservation_date],start_time: start_time)
+      flash[:notice] = "指定した時間帯はすでに予約が入っています"
+      redirect_to home_index_path
+    elsif @reservation_time.update(user_id: session[:user_id])
+      @reservation_time.save     
       flash[:notice] = "予約が完了しました"
       redirect_to home_index_path
     else
@@ -15,6 +19,10 @@ class ReservationTimesController < ApplicationController
 
   private
   def reservation_time_params
-    params.require(:reservation_time).permit(:reservation_theme,:reservation_date,:start_time,:end_time)  
+    params.require(:reservation_time).permit(:reservation_theme,
+                                             :reservation_date,
+                                             :start_time,
+                                             :end_time
+                                             )  
   end
 end
