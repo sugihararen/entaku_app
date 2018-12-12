9910058d8491e66d4f8bc92ab584c2472aa71178
sugihararen <sugihara@dptr.jp>
2018-12-12 16:19:12 +0900

    ログインpermit追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new; end
+  def new;
+    @user = User.new
+  end
 
   def create
-    @user = User.find_by(name: params[:name])
-    if @user&.authenticate(params[:password])
+    @user = User.find_by(name: session_params[:name])
+    if @user&.authenticate(session_params[:password])
       session[:user_id] = @user.id
       flash[:notice] = 'ログインしました'
       redirect_to home_index_url
     else
       flash.now[:error] = 'ユーザー名またはパスワードが間違っています'
+      @user = User.new(name: session_params[:name],password:session_params[:password])
       render :new
     end
   end
@@ -19,5 +22,11 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = 'ログアウトしました'
     redirect_to login_url
+  end
+
+  def session_params
+    params.require(:user).permit(
+      :password, :name
+    )
   end
 end
