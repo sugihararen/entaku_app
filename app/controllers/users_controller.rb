ad08a7b2327e799c97edf134998affb1c7fa19aa
sugihararen <sugihara@dptr.jp>
2018-12-20 10:33:55 +0900

    削除されたユーザーの予約を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,8 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:user_id])
     user.destroy
+    reservation = ReservationTime.where(user_id:params[:user_id])
+    reservation.delete_all
     flash[:notice] = "ユーザーを削除しました"
     redirect_to users_index_url
   end
