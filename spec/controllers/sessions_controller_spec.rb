d0e3aeda5be710f47a64eb6cf4ed8fa325637688
sugihararen <sugihara@dptr.jp>
2018-12-20 11:37:25 +0900

    ログインログアウトrspec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,27 +4,22 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe 'sessions#create' do
-    subject { post :create, params: { user: { name: 'sugihararen', password: 'ren19971121' } } }
-
-    context '正常なログイン情報でログインした場合' do
-      before do
-        create(:user)
-      end
-
-      it 'TOP画面へ遷移されること' do
-        expect(subject).to redirect_to home_index_url
-      end
-    end
-
-    context '異常なログイン情報でログインした場合' do
-      it 'ログイン画面へ遷移' do
-        expect(subject).to render_template :new
-      end
+  before do
+    create(:user)
+  end
+  context '正常なログイン情報でログインした場合' do
+    it 'TOP画面へ遷移されること' do
+      expect(post :create,params:{user:{name:'sugihararen',password:'ren19971121'}}).to redirect_to home_index_url
     end
   end
-
+  context '異常なログイン情報でログインした場合' do
+    it 'ログイン画面へ遷移' do
+      expect(post :create,params:{user:{name:'sugihararen',password:'ren1997112'}}).to render_template :new
+    end
+  end
+end
   describe 'sesions#destroy' do
-    context 'ログアウト'
+  context 'ログアウト'
     it 'ログイン画面へ遷移' do
       session = { 'user_id' => 'nil' }
       add_session(session)
--- spec/factories/uses.rb
+++ spec/factories/uses.rb
@@ -3,10 +3,9 @@
 FactoryBot.define do
   factory :user do
     name                   'sugihararen'
-    email                  'sugihara@dptr.jp'
+    email                  'smple@dptr.jp'
     status                 '管理者'
     password               'ren19971121'
     password_confirmation  'ren19971121'
-
   end
 end
