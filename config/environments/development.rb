e8192f3a145fbe485a94f58e0dec3367e685f93a
sugihararen <sugihara@dptr.jp>
2019-01-16 10:11:37 +0900

    メール本文修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,13 +65,4 @@ Rails.application.configure do
 
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
-  ActionMailer::Base.smtp_settings = {
-  :user_name => 'your_sendgrid_username',
-  :password => 'your_sendgrid_password',
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
 end
